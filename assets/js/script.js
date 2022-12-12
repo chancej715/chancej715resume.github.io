c9761493fe51de1003537e01bb31646e7d7f3354
Chance Johnson <chancej715@gmail.com>
2022-12-12 11:06:48 -0600

    Screen resizing support.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,17 @@ const sizes = {
     height: window.innerHeight
 }
 
+window.addEventListener('resize', () =>
+{
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+    
+    renderer.setSize(sizes.width, sizes.height)
+})
+
 // Scene
 const scene = new THREE.Scene()
 
